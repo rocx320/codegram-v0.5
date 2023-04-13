09eb1643270a52d5a88fb8223defc3b5b5c8e5d6
Piyush Rai <64735560+rocx320@users.noreply.github.com>
2023-04-13 10:37:04 +0530

    UI revamps

--- codegram/settings.py
+++ codegram/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'code3',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '3306',
     }
